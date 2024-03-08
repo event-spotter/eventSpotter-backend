97242c2e17fd483f6058fd607a36b038df13d95a
MelSchiffner <156306012+MelSchiffner@users.noreply.github.com>
2024-03-08 16:26:58 +0100

    fix bug with cloudinary

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-const fileUploader = require("../config/cloudinary.config");
-
 // ℹ️ Handles password encryption
 const bcrypt = require("bcrypt");
 
@@ -19,18 +17,6 @@ const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 // How many rounds should bcrypt run the salt (default - 10 rounds)
 const saltRounds = 10;
 
-
-// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
-router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
-  // console.log("file is: ", req.file)
-  if (!req.file) {
-    next(new Error("No file uploaded!"));
-    return;
-  }
-  // Get the URL of the uploaded file and send it as a response.
-  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
-  res.json({ imageUrl: req.file.path });
-});
 
 // POST /auth/signup  - Creates a new user in the database
 router.post("/signup", async (req, res, next) => {
@@ -61,14 +47,7 @@ router.post("/signup", async (req, res, next) => {
       return;
     }
     console.log("Signup request body:", req.body);
-    let imageUrl = null;
-    if (image) {
-      const cloudinaryResponse = await cloudinary.uploader.upload(image, {
-        folder: "users", 
-      });
-      console.log("Cloudinary response:", cloudinaryResponse);
-      imageUrl = cloudinaryResponse.secure_url;
-    }
+   
 
     // Check the users collection if a user with the same email already exists
     const foundUser = await User.findOne({ email });
@@ -86,7 +65,7 @@ router.post("/signup", async (req, res, next) => {
       email,
       password: hashedPassword,
       name,
-      image: imageUrl,
+      image
     });
 
     // Deconstruct the newly created user object to omit the password
